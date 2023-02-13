b4e1a792229d3cdd17b7d964d5b22d29c139a5de
201sdsadhsdsadha <100411446+201sdsadhsdsadha@users.noreply.github.com>
2023-02-13 19:37:50 +0800

    This is random 10,000

---
+++ 10000位随机数.py
@@ -0,0 +1,18 @@
+import turtle
+import random
+from tkinter import messagebox
+import tkinter
+messagebox.showinfo("Info","10,000 randon begin!")
+for i in range (1000):
+    num1 = random.randint(0,9)
+    num2 = random.randint(0,9)
+    num3 = random.randint(0,9)
+    num4 = random.randint(0,9)
+    num5 = random.randint(0,9)
+    num6 = random.randint(0,9)
+    num7 = random.randint(0,9)
+    num8 = random.randint(0,9)
+    num9 = random.randint(0,9)
+    num10 = random.randint(0,9)
+    print(num1,num2,num3,num4,num5,num6,num7,num8,num9,num10)
+messagebox.showinfo("Info","10,000 random ends, check the control console!")
